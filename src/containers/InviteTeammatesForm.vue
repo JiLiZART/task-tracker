12701ae1d3f67ae6cd529d58bb725f0c45dc107b
Nikolay Kostyurin <jilizart@gmail.com>
2017-08-25 11:58:16 +0200

    substract invite form from invite teammates page

---
+++ src/containers/InviteTeammatesForm.vue
@@ -0,0 +1,81 @@
+<template>
+  <form class="invite-teammates-form" @submit.prevent="onSubmit">
+    <template v-for="(item, index) in mates">
+      <label :for="item.name" class="sr-only">{{item.label}}</label>
+      <div class="input-group invite-teammates-form__input">
+        <input type="email" :id="item.name" class="form-control form-control-lg"
+               v-model="item.value"
+               v-focus="focused[index]"
+               @focus="focused[index] = true"
+               @blur="focused[index] = false"
+               :placeholder="item.label + ' Mail'"
+               :required="required[index]">
+        <span class="input-group-btn" v-if="isCanRemoveItem(index)">
+          <button class="btn btn-outline-danger btn-lg" type="button" @click="onRemoveItem(index)">
+            <i class="fa fa-times" aria-hidden="true"></i>
+          </button>
+        </span>
+      </div>
+    </template>
+
+    <button class="btn btn-lg btn-outline-primary btn-block" type="button" @click="onAdd">Add more</button>
+    <button class="btn btn-lg btn-success btn-block" type="submit">Invite</button>
+  </form>
+</template>
+
+<script>
+  import {mixin as focusMixin} from 'vue-focus';
+
+  export default {
+    name: 'invite-teammates-form',
+    mixins: [focusMixin],
+
+    props: {
+      submitRoute: {type: String}
+    },
+
+    data() {
+      return {
+        mates: [
+          {name: 'mate1', value: null, label: 'Mate 1'},
+          {name: 'mate2', value: null, label: 'Mate 2'},
+          {name: 'mate3', value: null, label: 'Mate 3'}
+        ],
+        focused: {0: true},
+        required: {0: true}
+      }
+    },
+    methods: {
+      isCanRemoveItem(index) {
+        return index > 2
+      },
+
+      onSubmit() {
+        for (let item of this.mates) {
+          if (item.value) {
+            this.$store.commit('inviteTeamMate', item.value);
+          }
+        }
+
+        this.$router.push(this.submitRoute);
+      },
+      onAdd() {
+        const idx = this.mates.length + 1;
+
+        this.mates.push({name: `mate${idx}`, value: null, label: `Mate ${idx}`},)
+      },
+
+      onRemoveItem(index) {
+        this.mates.splice(index, 1);
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .invite-teammates-form {
+    &__input {
+      margin-bottom: 10px;
+    }
+  }
+</style>
--- src/views/InviteTeammates.vue
+++ src/views/InviteTeammates.vue
@@ -5,59 +5,20 @@
         <h1 class="cover-heading">Invite Your Teammates.</h1>
       </div>
 
-      <form class="invite-teammates__form" @submit.prevent="onSubmit">
-        <label for="mate1" class="sr-only">Mate 1</label>
-        <input type="email" id="mate1" class="invite-teammates__input form-control form-control-lg"
-               v-model="mate1"
-               v-focus="focused"
-               @focus="focused = true" @blur="focused = false"
-               placeholder="Mate 1 Email" required autofocus>
-
-        <label for="mate2" class="sr-only">Mate 2</label>
-        <input type="email" id="mate2" class="invite-teammates__input form-control form-control-lg"
-               v-model="mate2"
-               placeholder="Mate 2 Email">
-
-        <label for="mate3" class="sr-only">Mate 3</label>
-        <input type="email" id="mate3" class="invite-teammates__input form-control form-control-lg"
-               v-model="mate3"
-               placeholder="Mate 3 Email">
-
-        <button class="btn btn-lg btn-success btn-block" type="submit">Invite</button>
-        <router-link class="btn btn-lg btn-link btn-block" to="create-project">Skip this step</router-link>
-      </form>
+      <div class="invite-teammates__body">
+        <invite-teammates-form submit-route="/create-project"></invite-teammates-form>
+        <router-link class="btn btn-lg btn-link btn-block" to="/create-project">Skip this step</router-link>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-  import {mixin as focusMixin}  from 'vue-focus';
+  import InviteTeammatesForm from '@/containers/InviteTeammatesForm'
 
   export default {
     name: 'invite-teammates-view',
-    mixins: [focusMixin],
-
-    data() {
-      return {
-        mate1: null,
-        mate2: null,
-        mate3: null,
-        focused: true
-      }
-    },
-
-    methods: {
-      onSubmit() {
-
-        for (let email of [this.mate1, this.mate2, this.mate3]) {
-          if (email) {
-            this.$store.commit('inviteTeamMate', email);
-          }
-        }
-
-        this.$router.push('create-project');
-      }
-    }
+    components: {InviteTeammatesForm},
   }
 </script>
 
@@ -82,7 +43,7 @@
       text-align: center;
     }
 
-    &__form {
+    &__body {
       max-width: 330px;
       width: 100%;
       margin: 0 auto;
