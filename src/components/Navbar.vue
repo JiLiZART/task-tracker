f245c20b553317344d0131ab715934213e3f2ea7
Nikolay Kostyurin <jilizart@gmail.com>
2017-09-04 00:36:18 +0200

    make navbar mobile friendly

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,39 +1,40 @@
 <template>
   <b-navbar toggleable type="inverse" variant="secondary">
-      <div class="navbar__workspace-title">
-        <router-link class="navbar-brand navbar__workspace-link" to="/dashboard">
-          {{ workspace.title }}
-        </router-link>
-      </div>
+    <div class="navbar__workspace-title">
+      <router-link class="navbar-brand navbar__workspace-link" to="/dashboard">
+        {{ workspace.title }}
+      </router-link>
+    </div>
 
-      <div class="navbar__workspaces" v-if="false">
-        <b-dropdown class="navbar__workspaces-dropdown" variant="transparent">
-          <i class="fa fa-desktop" slot="button-content"></i>
-          <template v-for="(item, index) in workspaces">
-            <b-dropdown-item
+    <div class="navbar__workspaces" v-if="false">
+      <b-dropdown class="navbar__workspaces-dropdown" variant="transparent">
+        <i class="fa fa-desktop" slot="button-content"></i>
+        <template v-for="(item, index) in workspaces">
+          <b-dropdown-item
             :disabled="isWorkspaceActive(item)" :key="item._id"
-            @click="changeWorkspace(item)">{{ item.title }}</b-dropdown-item>
-          </template>
-          <b-dropdown-divider></b-dropdown-divider>
-          <router-link class="dropdown-item" to="/create-workspace">Create Workspace</router-link>
-        </b-dropdown>
-      </div>
-
-      <form class="form-inline navbar__form">
-        <input class="form-control mr-sm-2 navbar__input"
-               :value="searchValue"
-               @input="onSearch"
-               placeholder="Search">
-      </form>
-
-      <b-dropdown class="navbar__user" variant="transparent" :right="true">
-        <author class="navbar__user-toggle" slot="button-content" :item="user" :haveLink="false"></author>
-
-        <router-link class="dropdown-item" to="/settings">Settings</router-link>
-        <span class="dropdown-item" @click="logout">Sign out</span>
+            @click="changeWorkspace(item)">{{ item.title }}
+          </b-dropdown-item>
+        </template>
         <b-dropdown-divider></b-dropdown-divider>
-        <span class="dropdown-item" @click="clear">Clear sandbox</span>
+        <router-link class="dropdown-item" to="/create-workspace">Create Workspace</router-link>
       </b-dropdown>
+    </div>
+
+    <form class="form-inline navbar__form">
+      <input class="form-control mr-sm-2 navbar__input"
+             :value="searchValue"
+             @input="onSearch"
+             placeholder="Search">
+    </form>
+
+    <b-dropdown class="navbar__user" variant="transparent" :right="true">
+      <author class="navbar__user-toggle" slot="button-content" :item="user" :haveLink="false"></author>
+
+      <router-link class="dropdown-item" to="/settings">Settings</router-link>
+      <span class="dropdown-item" @click="logout">Sign out</span>
+      <b-dropdown-divider></b-dropdown-divider>
+      <span class="dropdown-item" @click="clear">Clear sandbox</span>
+    </b-dropdown>
   </b-navbar>
 </template>
 
@@ -104,6 +105,17 @@
       padding: 0 .5rem;
     }
 
+    &__workspace-title {
+      max-width: 100px;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+
+      @media (min-width: 992px) {
+        max-width: 200px;
+      }
+    }
+
     &__workspaces-dropdown {
       height: 40px;
       padding: 0 .5rem;
@@ -112,6 +124,15 @@
     &__user {
       height: 40px;
       color: white;
+      margin-left: 10px;
+    }
+
+    &__user-toggle .author__name {
+      display: none;
+
+      @media (min-width: 992px) {
+        display: inline-block;
+      }
     }
 
     .dropdown-toggle,
