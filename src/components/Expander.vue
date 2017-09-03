fac561eefc1b1d60d548c15f384818f5a462b61d
Nikolay Kostyurin <jilizart@gmail.com>
2017-09-04 00:36:33 +0200

    move expand button to expander component

---
+++ src/components/Expander.vue
@@ -0,0 +1,34 @@
+<template>
+  <button class="btn btn-link expander" @click="toggleExpanded" tabindex="-1">
+    <i class="el-icon-arrow-up" v-if="expanded"></i>
+    <i class="el-icon-arrow-down" v-else></i>
+  </button>
+</template>
+
+<script>
+  export default {
+    name: 'expander',
+
+    props: ['expanded'],
+
+    methods: {
+      toggleExpanded() {
+        this.$emit('toggle')
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .expander {
+    padding: 0.5rem;
+    font-size: 1rem;
+    color: #818a91;
+    cursor: pointer;
+
+    &:hover, &:focus {
+      color: #818a91;
+      text-decoration: none;
+    }
+  }
+</style>
