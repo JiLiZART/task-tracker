0c7ee96cb37f22b5346d1814288efecf275b6c93
Vadim Cpp <vadim.cpp@gmail.com>
2017-12-16 04:46:46 +0200

    Fix layout
    Remove right margin on small screens for “New Task” and “New Bundle”

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -259,9 +259,13 @@
       text-align: center;
       margin-right: .5rem;
       cursor: pointer;
-      
+
       &:disabled {
         opacity: .35;
+      }
+
+      @media (max-width: 992px) {
+        margin-right: 0;
       }
     }
 
