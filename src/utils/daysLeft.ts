3f726b332cdd01d8fa22434b72dd5ebd19e5a0a5
Nikolay Kostyurin <jilizart@gmail.com>
2020-12-14 02:06:38 +0200

    refactor: move utils to ts

--- src/utils/daysLeft.ts
+++ src/utils/daysLeft.ts
@@ -5,8 +5,9 @@ export default function daysLeft(value) {
 
   const start = new Date();
   const end = new Date(value);
+  const daysCount = start.getTime() - end.getTime();
   const word = start < end ? " left" : " delay";
-  const days = `${start - end}`; //  moment(end).toNow(true);
+  const days = `${daysCount}`; //  moment(end).toNow(true);
 
   return days + word;
 }
